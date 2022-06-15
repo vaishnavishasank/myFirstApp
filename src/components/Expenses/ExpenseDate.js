d450b24c8c9ba26bf845be3c6c0749ae2914d8d9
vaishnavishasank <vaishnavimusale1999@gmail.com>
2022-06-15 12:33:59 -0400

    just test

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -6,7 +6,7 @@ const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
-  console.log("HI")
+  
   return (
     <div className='expense-date'>
       <div className='expense-date__month'>{month}</div>
